7364e040faad2312040463378c59df439d90defa
David Herrera <david@alleyinteractive.com>
2015-12-26 21:44:36 -0500

    Pattern archive date tag after `get_the_archive_title()`

--- php/class-wp-seo-format-archive-date.php
+++ php/class-wp-seo-format-archive-date.php
@@ -15,14 +15,16 @@ class WP_SEO_Format_Archive_Date extends WP_SEO_Formatting_Tag {
 		return __( 'Replaced with the date of the archive being viewed.', 'wp-seo' );
 	}
 
-	// @see the "_s" theme for these date strings.
+	/**
+	 * @see get_the_archive_title() for these date strings.
+	 */
 	public function get_value() {
-		if ( is_day() ) {
-			return get_the_date();
+		if ( is_year() ) {
+			return get_the_date( _x( 'Y', 'yearly archives title tag format', 'wp-seo' ) );
 		} elseif ( is_month() ) {
 			return get_the_date( _x( 'F Y', 'monthly archives title tag format', 'wp-seo' ) );
-		} elseif ( is_year() ) {
-			return get_the_date( _x( 'Y', 'yearly archives title tag format', 'wp-seo' ) );
+		} elseif ( is_day() ) {
+			return get_the_date( _x( 'F j, Y', 'daily archives title tag format', 'wp-seo' ) );
 		}
 
 		return false;
