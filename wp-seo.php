954beb75cbf535f9942ba00cff77d62488a77603
David Herrera <david@alleyinteractive.com>
2016-07-01 22:31:45 -0400

    Bump enqueued script version

--- wp-seo.php
+++ wp-seo.php
@@ -47,7 +47,7 @@ require_once WP_SEO_PATH . '/php/general-functions.php';
 require_once WP_SEO_PATH . '/php/default-filters.php';
 
 function wp_seo_admin_scripts() {
-	wp_enqueue_script( 'wp-seo-admin', WP_SEO_URL . 'js/wp-seo.js', array( 'jquery', 'underscore' ), '0.9.0', true );
+	wp_enqueue_script( 'wp-seo-admin', WP_SEO_URL . 'js/wp-seo.js', array( 'jquery', 'underscore' ), '0.11.1', true );
 	wp_localize_script( 'wp-seo-admin', 'wp_seo_admin', array(
 		'repeatable_add_more_label' => __( 'Add another', 'wp-seo' ),
 		'repeatable_remove_label' => __( 'Remove group', 'wp-seo' ),
