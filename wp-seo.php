4e9c2130ec21ae79024a3bedcd089cb09ff28b9b
David Herrera <david@alleyinteractive.com>
2016-08-19 18:29:03 -0400

    Make wp-seo.php coding-standards compliant
    - Update plugin header to act as a file header
    - Use proper block comment format for license
    - Add docs for `wp_seo_admin_scripts()`

--- wp-seo.php
+++ wp-seo.php
@@ -1,26 +1,28 @@
 <?php
+/**
+ * Plugin Name: WP SEO
+ * Plugin URI: https://github.com/alleyinteractive/wp-seo
+ * Description: An SEO plugin that stays out of your way. Just the facts, Jack.
+ * Version: 0.11.3-beta1
+ * Author: Alley Interactive, Matthew Boynes, David Herrera
+ * Author URI: http://www.alleyinteractive.com/
+ *
+ * @package WP_SEO
+ */
+
 /*
-	Plugin Name: WP SEO
-	Plugin URI: https://github.com/alleyinteractive/wp-seo
-	Description: An SEO plugin that stays out of your way. Just the facts, Jack.
-	Version: 0.11.3-beta1
-	Author: Alley Interactive, Matthew Boynes, David Herrera
-	Author URI: http://www.alleyinteractive.com/
-*/
-/*  This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-*/
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 
 define( 'WP_SEO_PATH', dirname( __FILE__ ) );
 define( 'WP_SEO_URL', trailingslashit( plugins_url( '', __FILE__ ) ) );
@@ -49,6 +51,9 @@ require_once WP_SEO_PATH . '/php/formatting-functions.php';
 // The plugin's default filters.
 require_once WP_SEO_PATH . '/php/default-filters.php';
 
+/**
+ * Enqueues scripts and styles for administration pages.
+ */
 function wp_seo_admin_scripts() {
 	wp_enqueue_script( 'wp-seo-admin', WP_SEO_URL . 'js/wp-seo.js', array( 'jquery', 'underscore' ), '0.11.1', true );
 	wp_localize_script( 'wp-seo-admin', 'wp_seo_admin', array(
