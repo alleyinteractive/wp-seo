c3d871ad2a2d0f64c1a37e810b459986310b265f
David Herrera <david@alleyinteractive.com>
2016-10-19 15:50:13 -0400

    Note filters allowing other `#thumbnail_url#` sizes

--- php/default-formatting-tags.php
+++ php/default-formatting-tags.php
@@ -566,6 +566,11 @@ class WP_SEO_Format_Thumbnail_URL extends WP_SEO_Formatting_Tag {
 	 * @return mixed Thumbnail URL, or false.
 	 */
 	public function get_value() {
+		/*
+		 * The {@see "wp_seo_format_{$id}"} filter is available for returning a
+		 * different image size. You also can subclass this formatting tag for
+		 * your needs and use the {@see 'wp_seo_formatting_tags'} filter.
+		 */
 		return ( is_singular() ) ? get_the_post_thumbnail_url( get_the_ID(), 'full' ) : false;
 	}
 }
