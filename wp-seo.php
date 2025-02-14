3f21a2201fbd0cb8782155c79d51c2ee3e70f205
Greg Marshall <greg@alley.co>
2025-02-14 15:50:21 -0600

    phpstan

--- wp-seo.php
+++ wp-seo.php
@@ -97,7 +97,7 @@ require_once WP_SEO_PATH . '/php/default-filters.php';
 /**
  * Enqueues scripts and styles for administration pages.
  */
-function wp_seo_admin_scripts() {
+function wp_seo_admin_scripts(): void {
 	wp_enqueue_script( 'wp-seo-admin', WP_SEO_URL . 'js/wp-seo.js', [ 'jquery', 'underscore' ], '0.11.1', true );
 	wp_localize_script( 'wp-seo-admin', 'wp_seo_admin', [
 		'repeatable_add_more_label' => __( 'Add another', 'wp-seo' ),
