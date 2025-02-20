de41dd141063cf18b1f9bfa1a885e2db4859a2bd
Matthew Ell <matt.ell@alley.com>
2025-02-20 14:23:56 -0600

    Add Open Graph tags rendering functionality

--- src/features/class-open-graph.php
+++ src/features/class-open-graph.php
@@ -20,6 +20,7 @@ final class Open_Graph implements Feature {
 	 */
 	public function boot(): void {
 		add_action( 'init', [ $this, 'add_meta_fields' ], 100 );
+		add_action( 'wp_head', [ $this, 'render_open_graph_tags' ] );
 	}
 
 	/**
@@ -60,6 +61,33 @@ final class Open_Graph implements Feature {
 				'type'              => 'integer',
 				'show_in_rest'      => true,
 			]
+		);
+	}
+
+	/**
+	 * Render Open Graph tags.
+	 */
+	public function render_open_graph_tags(): void {
+		$title	     = get_post_meta( get_the_ID(), 'wp_seo_open_graph_title', true ) ?? get_the_title();
+		$description = get_post_meta( get_the_ID(), 'wp_seo_open_graph_description', true ) ?? get_the_excerpt();
+		$image	     = ! empty( get_post_meta( get_the_ID(), 'wp_seo_open_graph_image', true ) )
+			? wp_get_attachment_image_url( get_post_meta( get_the_ID(), 'wp_seo_open_graph_image', true ), 'full' )
+			: get_the_post_thumbnail_url( get_the_ID(), 'full' );
+
+		printf(
+			<<<'HTML'
+<!-- Start Open Graph -->
+<meta property="og:type" content="website" />
+<meta property="og:title" content="%1$s" />
+<meta property="og:description" content="%2$s" />
+<meta property="og:url" content="%3$s" />
+%4$s
+<!-- End Open Graph -->
+HTML,
+			esc_attr( $title ),
+			esc_attr( $description ),
+			esc_url( get_permalink() ),
+			! empty ( $image ) ? sprintf( '<meta property="og:image" content="%s" />', esc_url( $image ) ) : ''
 		);
 	}
 }
