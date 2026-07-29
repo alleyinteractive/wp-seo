e8117a3e4a2625c4c1e22112c9ba893a125ec7c8
Willow Celeste <54474119+willowCeleste@users.noreply.github.com>
2026-07-29 10:12:03 -0400

    fixes failing 8.3 php test

--- tests/Feature/PropertiesTest.php
+++ tests/Feature/PropertiesTest.php
@@ -28,7 +28,7 @@ class PropertiesTest extends TestCase {
 	}
 
 	function _add_mock( $tags ) {
-		$tags['is_a_tag'] = $this->getMockBuilder( 'WP_SEO_Formatting_Tag' )->getMockForAbstractClass();
+		$tags['is_a_tag'] = $this->getMockBuilder( 'WP_SEO_Formatting_Tag' )->getMock();
 
 		return $tags;
 	}
